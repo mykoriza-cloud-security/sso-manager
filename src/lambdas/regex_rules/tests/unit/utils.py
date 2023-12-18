f2835212d1c4c754105c7be5f1de4c9eaf296f16
islamelkadi <ielkadi@torontomu.ca>
2023-12-18 11:36:22 -0500

    Remove unnecessary error messages

--- src/lambdas/regex_rules/tests/unit/utils.py
+++ src/lambdas/regex_rules/tests/unit/utils.py
@@ -3,8 +3,6 @@
 # Global vars
 COMMON_ERROR_MESSAGES = {
     "list_size": "Expected different list size",
-    "http_status_code": "Unexpected HTTP status code",
-    "response_message": "Unexpected response message",
     "regex_pattern": "Item does not match expected Regex pattern",
     "ddb_numerical": "Incorrect numerical DynamoDB item datatype",
     "hash_key": "Processed hash key name is different from desired",
