88b28fdbe50ebcc6cd34b70af0e72aa9cf86d513
islamelkadi <ielkadi@torontomu.ca>
2024-04-22 01:11:35 -0400

    WIP

--- tests/unit/test_organizations.py
+++ tests/unit/test_organizations.py
@@ -6,6 +6,7 @@ import json
 import moto
 import boto3
 import pytest
+from pprint import pprint
 
 ################################################
 #                    Fixture                   #
@@ -28,11 +29,13 @@ def organizations_client(set_aws_creds):
     with moto.mock_organizations():
         yield boto3.client("organizations")
 
-
+@moto.mock_organizations
 @pytest.fixture(autouse=True)
 def create_aws_organization(get_organization_map, organizations_client):
-    parent_ou_id = organizations_client.create_organization()["Organization"]["Id"]
-    create_aws_ous_accounts(get_organization_map, parent_ou_id)
+    organizations_client.create_organization()
+    root_ou_id = organizations_client.list_roots()["Roots"][0]["Id"]
+    aws_organization_definitions = get_organization_map["organization_definition"]
+    create_aws_ous_accounts(organizations_client, aws_organization_definitions, root_ou_id)
 
 
 ################################################
@@ -41,7 +44,7 @@ def create_aws_organization(get_organization_map, organizations_client):
 
 
 def create_aws_ous_accounts(
-    aws_organizations_map: dict, root_ou_id: str, parent_ou_id: str = ""
+    organizations_client, aws_organization_definitions: list, root_ou_id: str, parent_ou_id: str = ""
 ) -> None:
     """
     Fixture to setup AWS mock organizations by creating:
@@ -50,7 +53,8 @@ def create_aws_ous_accounts(
         3. Creating AWS accounts
         4. Moving those AWS accounts under their designated OU
     """
-    for organization_resource in aws_organizations_map:
+    for organization_resource in aws_organization_definitions:
+
         if organization_resource["type"] == "ORGANIZATIONAL_UNIT":
             # Create OU
             nest_ou_id = organizations_client.create_organizational_unit(
@@ -61,29 +65,33 @@ def create_aws_ous_accounts(
             # Recursively setup OU
             if organization_resource["children"]:
                 create_aws_ous_accounts(
-                    organization_resource["children"], root_ou_id, nest_ou_id
+                    organizations_client, organization_resource["children"], root_ou_id, nest_ou_id
                 )
 
-        # Create account
-        account_id = organizations_client.create_account(
-            Email=f"{organization_resource['name']}@testing.com",
-            AccountName=organization_resource["name"],
-        )["CreateAccountStatus"]["AccountId"]
+        elif organization_resource["type"] == "ACCOUNT":
+            # Create account
+            account_id = organizations_client.create_account(
+                Email=f"{organization_resource['name']}@testing.com",
+                AccountName=organization_resource["name"],
+            )["CreateAccountStatus"]["AccountId"]
 
-        # Move account to OU
-        organizations_client.move_account(
-            AccountId=account_id,
-            SourceParentId=root_ou_id,
-            DestinationParentId=parent_ou_id,
-        )
+            # Move account to OU
+            organizations_client.move_account(
+                AccountId=account_id,
+                SourceParentId=root_ou_id,
+                DestinationParentId=parent_ou_id,
+            )
+    
+    print("GRRRRRR")
+    pprint(organizations_client.list_accounts()["Accounts"])
 
 
-# def test_get_active_aws_accounts_include_all_organiational_units(get_organizations_details) -> None:
-#     pass
+def test_get_active_aws_accounts_include_all_organiational_units(create_aws_organization) -> None:
+    pass
 
-# def test_get_active_aws_accounts_exclue_specific_organizational_units(get_organizations_details) -> None:
-#     pass
+# # def test_get_active_aws_accounts_exclue_specific_organizational_units(get_organizations_details) -> None:
+# #     pass
 
-# @moto.mock_organizations
-# def test_get_active_aws_accounts_filter_suspended(organizations_client, get_organizations_details) -> None:
-#     pass
+# # @moto.mock_organizations
+# # def test_get_active_aws_accounts_filter_suspended(organizations_client, get_organizations_details) -> None:
+# #     pass
