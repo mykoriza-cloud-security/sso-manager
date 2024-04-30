d61fdd2323d53299fbbf662b646238f4068cc62e
islamelkadi <ielkadi@torontomu.ca>
2024-04-30 00:00:31 -0400

    Update test_index.py

--- tests/unit/test_index.py
+++ tests/unit/test_index.py
@@ -3,7 +3,6 @@ Unit tests to test writing regex rules from DDB
 """
 
 # Imports
-import os
 import importlib
 import pytest
 from aws_lambda_powertools.utilities.data_classes import EventBridgeEvent
@@ -17,16 +16,15 @@ def test(
 ) -> None:
     
     # Arrange
-    root_ou_id = setup_aws_organization["root_ou_id"]
+    context = generate_lambda_context()
     monkeypatch = pytest.MonkeyPatch()
-    monkeypatch.setenv("ROOT_OU_ID", root_ou_id)
+    monkeypatch.setenv("ROOT_OU_ID", setup_aws_organization["root_ou_id"])
 
     from src.app import index
     importlib.reload(index)
-    context = generate_lambda_context()
-    
+
     # Act
     lambda_response = index.lambda_handler(EventBridgeEvent(data={}), context)
-    print(lambda_response)
+    print(lambda_response.body)
 
     # Assert
